a8d923f3010beebe284b80ed7e4e6733b5f71dca
kon5271220000 <kon527122000@gmail.com>
2023-02-08 16:02:22 +0700

    delete jump

--- BrokenSword/Assets/Scripts/PlayerController.cs
+++ BrokenSword/Assets/Scripts/PlayerController.cs
@@ -20,14 +20,6 @@ public class PlayerController : MonoBehaviour
     private float _verticalDirection;
     private bool _changingDirection => (_rb.velocity.x > 0f && _horizontalDirection < 0f) || (_rb.velocity.x < 0f && _horizontalDirection > 0f);
 
-    [Header("Jump Variables")]
-    [SerializeField] private float _jumpForce = 12.0f;
-
-    [Header("Ground Collision Variables")]
-    [SerializeField] private float _groundRaycastLength;
-    private bool _onGround;
-    private bool _canJump => Input.GetButtonDown("Jump") && _onGround;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -42,10 +34,8 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        CheckCollisions();
         MoveCharactor();
         ApplyLinearDrag();
-        if (_canJump) Jump();
     }
 
     private Vector2 GetInput()
@@ -73,22 +63,5 @@ public class PlayerController : MonoBehaviour
         {
             _rb.drag = 0.0f;
         }
-    }
-
-    private void Jump()
-    {
-        _rb.velocity = new Vector2(_rb.velocity.x, 0f);
-        _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
-    }
-
-    private void CheckCollisions()
-    {
-        _onGround = Physics2D.Raycast(transform.position * _groundRaycastLength, Vector2.down, _groundRaycastLength, _groundLayer);
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * _groundRaycastLength);
     }
 }
